b33dcd51fa6c78e56f9a46163db71c2b66a318a6
valentin.belousov <belousowork@gmail.com>
2020-04-01 16:35:32 +0300

    update index

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+const path = require('path')
 const PORT = process.env.PORT || 3000;
 
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+/*
 app.get('/', (req,res) =>{
     res.send('<h1>Hello!!!!</h1>');
-});
+});*/
 
 
 app.listen(PORT, function(){
