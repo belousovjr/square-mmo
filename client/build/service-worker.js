bc4f8b5c7661d59fef32a446c31c09d38c8722c1
valentin.belousov <belousowork@gmail.com>
2020-04-02 00:54:06 +0300

    commit eee

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.99232a491f4469ce8b2fdbcea56a6541.js"
+  "/precache-manifest.9383a8fd26749ea7c40d9d03ee88aee1.js"
 );
 
 self.addEventListener('message', (event) => {
